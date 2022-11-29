4189987ec3d430a8895a5a4eb397fe16ae436795
Dmitry Lutsishin <graf.luck@gmail.com>
2022-11-29 15:36:55 +0300

    refactor: разбил код на модули

---
+++ js/constant.js
@@ -0,0 +1,8 @@
+const Constant = {
+  COUNT_DISPLAY_MARKERS: 10,
+  GEOGRAPHIC_COORDINATE_ACCURACY: 5,
+};
+
+const getConstant = () => Constant;
+
+export {getConstant};
---
+++ js/util.js
@@ -0,0 +1,42 @@
+const generateRandomInteger = (from, to) => {
+  if (typeof from !== 'number' || typeof to !== 'number') {
+    return NaN;
+  }
+
+  if (from < 0 || to < 0) {
+    return NaN;
+  }
+
+  if (from === to) {
+    return from;
+  }
+
+  if (to < from) {
+    [from, to] = [to, from];
+  }
+
+  return Math.round(Math.random() * (to - from) + to);
+};
+
+const generateRandomFloat = (from, to, precision) => {
+  if (typeof from !== 'number' || typeof to !== 'number' || typeof precision !== 'number') {
+    return NaN;
+  }
+
+  if (from < 0 || to < 0 || precision < 0) {
+    return NaN;
+  }
+
+  if (from === to) {
+    return from;
+  }
+
+  if (to < from) {
+    [from, to] = [to, from];
+  }
+
+  const powerPrecision = Math.pow(10, precision);
+  return +(Math.round((Math.random() * (to - from) + from) * powerPrecision) / powerPrecision).toFixed(precision);
+};
+
+export {generateRandomInteger, generateRandomFloat};
